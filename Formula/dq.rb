cbbffa573ea3f0f6eaa91329c1c53f3e075c0fe3
Ryan Miville <ryanmiville@gmail.com>
2026-08-17 21:33:32 -0400

    ci: build dq bottles for Intel macOS

--- Formula/dq.rb
+++ Formula/dq.rb
@@ -1,5 +1,5 @@
 class Dq < Formula
-  desc "Small CLI for data pipelines powered by DuckDB"
+  desc "Shell-first data pipelines powered by DuckDB"
   homepage "https://github.com/ryanmiville/dq"
   url "https://github.com/ryanmiville/dq/archive/refs/tags/v0.6.0.tar.gz"
   sha256 "9ba30d3fd1f3b8f16e45ddd27cc32e51e3c72228478c143fd6fb32f2fe677569"
