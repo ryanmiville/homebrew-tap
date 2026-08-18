5a31882a45e218d027e7accfecd824eeb9738255
ryanmiville <2359050+ryanmiville@users.noreply.github.com>
2026-08-18 22:02:00 +0000

    Update dq to v0.7.1
    Closes #10.
    
    Signed-off-by: Ryan Miville <2359050+ryanmiville@users.noreply.github.com>

--- Formula/dq.rb
+++ Formula/dq.rb
@@ -1,8 +1,8 @@
 class Dq < Formula
   desc "Shell-first data pipelines powered by DuckDB"
   homepage "https://github.com/ryanmiville/dq"
-  url "https://github.com/ryanmiville/dq/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "5f3adbc9c71230d3e1503bfc8b96027775c6d5b304842ed5423a7f58330d1298"
+  url "https://github.com/ryanmiville/dq/archive/refs/tags/v0.7.1.tar.gz"
+  sha256 "72de0fe2ed527bb106a1e929f9cb74380ac8035d9fe8ce808090f7b9abf13af2"
   license "MIT"
 
   depends_on "rust" => :build
