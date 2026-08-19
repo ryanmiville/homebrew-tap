a0e5b2e4d68d6126a9954ad6dac2026224062059
Ryan Miville <2359050+ryanmiville@users.noreply.github.com>
2026-08-19 00:27:19 +0000

    dq: add 0.7.1 bottle.

--- Formula/dq.rb
+++ Formula/dq.rb
@@ -5,6 +5,13 @@ class Dq < Formula
   sha256 "72de0fe2ed527bb106a1e929f9cb74380ac8035d9fe8ce808090f7b9abf13af2"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/ryanmiville/homebrew-tap/releases/download/dq-0.7.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ab23786292f9412409e7829edf0ba6e9f43253200cf9fceab4c0518950ba7c0"
+    sha256 cellar: :any_skip_relocation, sequoia:       "c208b45e144ad75a2093196e40164c6b9765b5af710e880f18c7cf182530e1a4"
+    sha256 cellar: :any,                 x86_64_linux:  "7335defba7311c75b58776eab35bc6bba1f482f1932aaade6c223f4fa01e4270"
+  end
+
   depends_on "rust" => :build
 
   def install
